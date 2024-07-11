a76a2ffac2a4ec83e5068a87f6f807898e9d384e
Reem Jarrar <99574238+ReemJ23@users.noreply.github.com>
2024-07-11 23:34:57 +0300

    Flash List and Flash card parts

--- flashcards/src/App.js
+++ flashcards/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
+import FlashcardList from './FlashcardList';
 
 function App() {
+  const [flashcards, setFlashcards]=useState(SAMPLE_FLASHCARDS)
   return (
-    <h1>Hello World</h1>
+    <FlashcardList flashcards={flashcards}/>
   );
 }
 
+const SAMPLE_FLASHCARDS =[{
+  id:1,
+  question: "What is 2+2 ?",
+  answer: "4",
+  options:['2','3','4','5']
+},
+{
+  id:2,
+  question: "What is 2+3 ?",
+  answer: "5",
+  options:['2','3','4','5']
+}]
 export default App;
